cf2da9e7be16877e42e66bfbcfa00ece26d9e77b
Tony Pan <tony.j.pan@gmail.com>
2025-03-24 15:39:08 -0700

    log errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+
+Sentry.init({
+  dsn: "https://04851436c4f309ca0eec04b2f760573a@o4509035101028352.ingest.us.sentry.io/4509035102666752",
+});
 
 const root = document.getElementById("root");
 if (root != null) {
